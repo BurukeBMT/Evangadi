1f7246b33f17ff63f932d116b49cef02533df6c9
BurukeBMT <burukmaedot16@gmail.com>
2025-07-08 21:15:22 -0400

    refactoring the code

--- Phase-2/Week-13/jQuery-Practice/JS/script.js
+++ Phase-2/Week-13/jQuery-Practice/JS/script.js
@@ -1,7 +1,13 @@
-// Question 1: The following three questions are based on the two paragraphs under the section which says, "For Question 1". 
-// 1.1. Select the element with an id of "sample1" using jQuery. 
-// 1.2. Print the element itself on the console upon page refresh. 
-// 1.3. Print the content of the element on the console upon page refresh. Use jQuery to select the content of the element
+/*
+Question 1: 
+The following three questions are based on the two paragraphs under the section which says, 
+"For Question 1". 
+1.1.  Select the element with an id of "sample1" using jQuery. 
+1.2.  Print the element itself on the console upon page refresh. 
+1.3.  Print the content of the element on the console upon page refresh. Use jQuery 
+to select the content of the element
+*/ 
+
 // 1.1: Select the element
   let sampleElement = $("#sample1");
 
@@ -12,15 +18,20 @@
   console.log(sampleElement.html()); // If you want HTML content
   console.log(sampleElement.text()); // If you want plain text content
 
-//   Question 2: The following questions are based on the HTML code found under the section labeled "For question 2".
-//    2.1. Select the element with an ID of "techCompanies" and display it on your console. 
-//    2.2. Howmanytech companies are listed under the ul element with an id of "techCompanies"? 
-//    2.3. Select all elements with a class of "red" and display them on the console. 
-//    2.4. Create a new li HTML element with a content of "Facebook" and display it on console 
-//    2.5. Give the newly created element a class of "blue" using jQuery 
-//    2.6. Append the newly created element next to the the "Sony" <li> element 
-//    2.7. Howmanyof the tech companies are labeled blue? Find the result using jQuery and display the result inside the "blueCompanies" div
-
+/*
+Question 2: 
+The following questions are based on the HTML code found under the section labeled "For 
+question 2". 
+2.1. Select the element with an ID of "techCompanies" and display it on your console. 
+2.2.  How many tech companies are listed under the ul element with an id 
+of "techCompanies"? 
+2.3. Select all elements with a class of "red" and display them on the console. 
+2.4. Create a new li HTML element with a content of "Facebook" and display it on console 
+2.5. Give the newly created element a class of "blue" using jQuery 
+2.6. Append the newly created element next to the the "Sony" <li> element 
+2.7.  How many of the tech companies are labeled blue? Find the result using jQuery and 
+display the result inside the "blueCompanies" div.   
+*/
 let techCompanies = $("#techCompanies");
   console.log(techCompanies);
 
@@ -48,13 +59,13 @@ let techCompanies = $("#techCompanies");
   $("#blueCompanies").text(`There are ${blueCompaniesCount} blue companies.`);
 
 
-// Question 3: A form with two text fields is provided under the section which says "For question 3". Write a jQuery code which takes the values of the two fields, checks if they are number values and calculate the sum and average of the two numbers. 
-// 3.1. Display the result on the console
-//  3.2. Display the result underneath the form
-//  3.3. If any of the numbers provided is not a number, display a message that says "Please enter numerical values only" underneath the form
-
-
-  
+/*
+Question 3:
+A form with two text fields is provided under the section which says "For question 3". Write a jQuery  code  which  takes  the values of the two fields, checks if they are number values and calculate the sum and average of the two numbers. 
+3.1.  Display the result on the console 
+3.2.  Display the result underneath the form 
+3.3.  If any of the numbers provided is not a number, display a message that says "Please enter numerical values only" underneath the form 
+*/
     
     $("#submitButton").click(function (event) {
       event.preventDefault(); // Prevent the form from submitting
@@ -88,7 +99,17 @@ let techCompanies = $("#techCompanies");
       }
     });
   
-// Question 4: Create an HTML form which asks users to provide their First name, Last name and Email address. All the fields should be labeled as required. Once the user submits, write a JavaScript function that checks if all the fields are provided. If not, it should show an error message above the form. If the user provides all the values, hide the form input fields, and display all the values provided by the user on the browser.
+/* 
+Question 4: 
+Create  an  HTML  form  which  asks  users  to  provide  their  First  name,  Last name and Email 
+address. All the fields should be labeled as required. Once the user submits, write a JavaScript 
+function that checks if all the fields are provided. If not, it should show an error message above 
+the form. 
+ 
+If the user provides all the values, hide the form input fields, and display all the values provided 
+by the user on the browser.
+*/
+
 $(document).ready(function () {
   $("#userForm").on("submit", function (event) {
     event.preventDefault(); // Prevent the form from submitting the traditional way
@@ -103,10 +124,14 @@ $(document).ready(function () {
     let form = $("#userForm");
     let userData = $("#userData");
 
-    // Check if all the fields are filled
+    // Regex for only letters (no numbers or special characters)
+    let nameRegex = /^[A-Za-z]+$/;
+
+    // Check if all the fields are filled and names are valid
     if (firstName === "" || lastName === "" || email === "") {
-      // Show the error message if any field is empty
-      errorMessage.show();
+      errorMessage.text("All fields are required.").show();
+    } else if (!nameRegex.test(firstName) || !nameRegex.test(lastName)) {
+      errorMessage.text("First name and Last name must contain only letters.").show();
     } else {
       // Hide the form and the error message
       errorMessage.hide();
@@ -123,6 +148,3 @@ $(document).ready(function () {
     }
   });
 });
-
-
-  

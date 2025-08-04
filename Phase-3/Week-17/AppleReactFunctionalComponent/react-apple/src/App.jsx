6deeffb3d3b049c6dda77d78ebbdf16dd60969ee
BurukeBMT <burukmaedot16@gmail.com>
2025-08-04 15:52:11 -0400

    customize styles

--- Phase-3/Week-17/AppleReactFunctionalComponent/react-apple/src/App.jsx
+++ Phase-3/Week-17/AppleReactFunctionalComponent/react-apple/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Alert from "./functionalComponents/Alert"
+import Fifthsection from "./functionalComponents/Fifthsection"
+import Firstsection from "./functionalComponents/Firstsection"
+import Footer from "./functionalComponents/Footer"
+import Fourthsection from "./functionalComponents/Fourthsection"
+import Header from "./functionalComponents/Header"
+import Secondsection from "./functionalComponents/Secondsection"
+import Sixthsection from "./functionalComponents/Sixthsection"
+import Thirdsection from "./functionalComponents/Thirdsection"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header/>
+      <Alert/>
+      <Firstsection/>
+      <Secondsection/>
+      <Thirdsection/>
+      <Fourthsection/>
+     <Fifthsection/>
+     <Sixthsection/> 
+     <Footer/>
     </>
   )
 }

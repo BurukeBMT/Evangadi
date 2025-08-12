45615f8cd521fd437544bebc457fb8a15565b78a
BurukeBMT <burukmaedot16@gmail.com>
2025-08-12 11:50:16 -0400

    Implement Evangadi Menu with food items and styling

--- Phase-3/Week-18/EvangadiMenu/evangadi-menu/src/App.js
+++ Phase-3/Week-18/EvangadiMenu/evangadi-menu/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// ...existing code...
+
+import './commonResource/CSS/style.css';
+import FoodItem from './FoodItem';
+import foodData from './commonResource/JS/data';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app-container">
+        <h1 className="menu-title">Evangadi Menu</h1>
+        <div className="food-list">
+          {foodData.map((item, idx) => (
+            <FoodItem
+              key={idx}
+              title={item.title}
+              img={item.img}
+              desc={item.desc}
+              price={item.price}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;

c90ddc54e93568e036f80055acf95eb778d8edaa
BurukeBMT <burukmaedot16@gmail.com>
2025-09-17 14:39:30 -0400

    feat: Initialize Netflix clone project with essential components and styling
    - Added main application structure in main.jsx
    - Created Home page with Header, Banner, RowList, and Footer components
    - Implemented Axios instance for API requests
    - Defined API requests for fetching movies and TV shows
    - Added global styles in index.css and imported Google Fonts
    - Configured Vite for React application
    - Included Netflix logo image asset

---
+++ Phase-4/Netflix-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../../Component/Header/Header'
+import Footer from '../../Component/Footer/Footer'
+import Banner from '../../Component/Banner/Banner'
+import RowList from '../../Component/Row/RowList/RowList'
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        < Header/>
+        < Banner/>
+        <RowList/>
+        < Footer/>
+
+        
+      </div>
+    </>
+  )
+}
+
+export default Home

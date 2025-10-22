6f3e32159873648099cef09467354b79ec39f0aa
BurukeBMT <burukmaedot16@gmail.com>
2025-10-22 06:01:04 -0400

    Add Evangadi Forum and Netflix clone projects

---
+++ Phase-4/Evangadi-Forum-Deployed/db/dbConfig.js
@@ -0,0 +1,21 @@
+const mysql2 = require("mysql2");
+require("dotenv").config();
+
+const dbConnection = mysql2.createPool({
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  connectionLimit: 10,
+});
+
+dbConnection.execute("SELECT 'test'", (err, result) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(result);
+  }
+});
+
+module.exports = dbConnection.promise();
+// module.exports = dbConnection;
